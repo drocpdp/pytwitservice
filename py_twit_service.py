faa043e2ff24181f74d9bd5ace06c83b7ed45b35
David Eynon <davideynon@davids-macbook-pro.home>
2022-10-15 17:12:58 +0200

    decoupled get_api() from main.py to return api object. This will allow for different auths later on

--- py_twit_service.py
+++ py_twit_service.py
@@ -12,17 +12,15 @@ import tweepy
 
 class PyTwitService(base_class.BaseClass):
     
-    def main(self): 
-
+    def get_api(self):
         try:
-            acct = AccountAccess()
-            api_key = acct.get_api_key()
-            api_secret = acct.get_api_key_secret()
-            access_token = acct.get_access_token()
-            access_token_secret = acct.get_access_token_secret()
-            bearer_token = acct.get_bearer_token()
-            oauth_2_0_client_id = acct.get_oauth_2_0_client_id()
-            oauth_2_0_client_secret = acct.get_oauth_2_0_client_secret()
+            api_key = AccountAccess().get_api_key()
+            api_secret = AccountAccess().get_api_key_secret()
+            access_token = AccountAccess().get_access_token()
+            access_token_secret = AccountAccess().get_access_token_secret()
+            bearer_token = AccountAccess().get_bearer_token()
+            oauth_2_0_client_id = AccountAccess().get_oauth_2_0_client_id()
+            oauth_2_0_client_secret = AccountAccess().get_oauth_2_0_client_secret()
         except Exception as e:
             self.log('ERROR: Trouble accessing account info')
             self.log(e)
@@ -33,20 +31,25 @@ class PyTwitService(base_class.BaseClass):
             api = tweepy.API(auth)
         except Exception as e:
             self.log('ERROR: Trouble with Authorization')
-            self.log(e)
+            self.log(e)        
+        return api
+
+    def main(self): 
+
+        api = self.get_api()
 
         filter = FilterTweets()
         twitter_actions = TwitterActions()
         
         #Get last processed tweet from serialized location
-        last_tweet_full_file_name = acct.get_last_tweet_id_location()
+        last_tweet_full_file_name = AccountAccess().get_last_tweet_id_location()
 
         if not os.path.isdir(os.path.dirname(last_tweet_full_file_name)):
             os.makedirs(os.path.dirname(last_tweet_full_file_name))
         if not os.path.isfile(last_tweet_full_file_name):
             last_processed_twitter_id = None
         else:
-            twit_id_file = open(acct.get_last_tweet_id_location(), 'r')
+            twit_id_file = open(AccountAccess().get_last_tweet_id_location(), 'r')
             last_processed_twitter_id = twit_id_file.read().strip()
             twit_id_file.close()
         
@@ -96,7 +99,7 @@ class PyTwitService(base_class.BaseClass):
         file_obj.write(str(since_id))
         file_obj.close()
         
-        email_log = acct.get_email_log()
+        email_log = AccountAccess().get_email_log()
         if email_log:
             Emailer().send_email(email_log)
 
